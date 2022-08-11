30c82f5d915a13a3fb9ae006a8133f34074298ba
league <dgyclark@gmail.com>
2022-08-11 16:59:52 -0700

    goof names

--- src/_01_algorithms/_2_fibonacci/fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/fibonacci.java
@@ -1,5 +1,19 @@
 package _01_algorithms._2_fibonacci;
 
 public class fibonacci {
+public static void main(String[] args) {
 
+
+int secondLast = 0 ; 
+ int last = 1;
+System.out.println(secondLast);
+System.out.println(last);
+for (int i = 0; i < 9; i++) {
+	int next = secondLast + last;
+	System.out.println(next);
+	secondLast = last;
+	last = next; 
+
+}
+}
 }
